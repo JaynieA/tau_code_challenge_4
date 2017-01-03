c601069935a4919018803a008153963cbb3ffa0e
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-03 15:40:00 -0600

    post route working

--- routes/treats.js
+++ routes/treats.js
@@ -12,8 +12,6 @@ router.get('/', function (req, res) {
       res.sendStatus(500);
       done();
       return;
-      /** ---- YOUR CODE BELOW ---- **/
-      // Add pg and pSQL code here to get treats from the treatDB
     } else {
       var query = client.query('SELECT * FROM treats');
       query.on('row', function(row) {
@@ -30,6 +28,23 @@ router.get('/', function (req, res) {
 /** ---- YOUR CODE BELOW ---- **/
 
 // POST /treats
+router.post('/', function(req, res) {
+  pg.connect(connectionString, function(err, client, done) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('connected to db');
+      //unpack values from req.body
+      var name = req.body.name;
+      var description = req.body.description;
+      var pic = req.body.pic;
+      var query = client.query('INSERT INTO treats (name, description, pic) VALUES ($1, $2, $3)', [name, description, pic]);
+      query.on('end', function() {
+        res.sendStatus(201);
+      }); // end query on
+    } // end else
+  }); // end pg connect
+}); // end post
 
 // PUT /treats/<id>
 
